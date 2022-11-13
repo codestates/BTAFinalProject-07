81ec759efb488e2dfe51526e5a348080a65ad656
choekko <choekko@gmail.com>
2022-11-13 21:05:08 +0900

    [#16] FEAT: GNB에 검색 바 추가

--- explorer/src/components/Navigation.tsx
+++ explorer/src/components/Navigation.tsx
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css, Theme } from '@emotion/react';
 import { Link, useLocation } from 'react-router-dom';
+import useSearchInput from '@/hooks/useSearchInput';
 
 function Navigation() {
   const location = useLocation();
+  const { inputValue, handleInputChange, handleSearchBtnClick } = useSearchInput();
 
   return (
     <nav css={navCss}>
@@ -15,21 +17,77 @@ function Navigation() {
             <span className="logo-text-scope">SCOPE</span>
           </div>
         </div>
-        <div css={linkGroupCss}>
-          <Link to={'/'}>
-            <span css={theme => linkCss(theme, location.pathname === '/')}>Home</span>
-          </Link>
-          <Link to={'/transactions'}>
-            <span css={theme => linkCss(theme, location.pathname === '/transactions')}>Transactions</span>
-          </Link>
-          <Link to={'/blocks'}>
-            <span css={theme => linkCss(theme, location.pathname === '/blocks')}>Blocks</span>
-          </Link>
-        </div>
+        <span css={rightWrapCss}>
+          {location.pathname !== '/' && (
+            <span css={searchInputWrapCss}>
+              <input
+                css={searchInputCss}
+                placeholder={'Search by Account ID / Txn Hash / Block'}
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              <button css={searchBtnCss} onClick={handleSearchBtnClick}>
+                <img width={15} height={15} src={'/assets/icon-search.png'} />
+              </button>
+            </span>
+          )}
+          <div css={linkGroupCss}>
+            <Link to={'/'}>
+              <span css={theme => linkCss(theme, location.pathname === '/')}>Home</span>
+            </Link>
+            <Link to={'/transactions'}>
+              <span css={theme => linkCss(theme, location.pathname === '/transactions')}>Transactions</span>
+            </Link>
+            <Link to={'/blocks'}>
+              <span css={theme => linkCss(theme, location.pathname === '/blocks')}>Blocks</span>
+            </Link>
+          </div>
+        </span>
       </div>
     </nav>
   );
 }
+
+const rightWrapCss = css`
+  display: flex;
+  gap: 40px;
+`;
+
+const searchInputWrapCss = (theme: Theme) => css`
+  display: flex;
+  width: 400px;
+  height: 30px;
+  border: 1px solid ${theme.color.orange200};
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+const searchInputCss = (theme: Theme) => css`
+  flex: 1;
+  color: ${theme.color.black600};
+  padding: 0 10px;
+  height: 100%;
+  outline: none;
+  border: none;
+
+  &::placeholder {
+    color: ${theme.color.black300};
+  }
+
+  &:focus::placeholder {
+    color: transparent;
+  }
+`;
+
+const searchBtnCss = (theme: Theme) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  background-color: ${theme.color.orange100};
+  border-left: 1px solid ${theme.color.orange200};
+`;
 
 const navCss = css`
   width: 100%;
